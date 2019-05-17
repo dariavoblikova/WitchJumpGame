a8ea7cd4f02c774f7e474644692d2c9f2cc6eff3
Joe Villegas <joev@uw.edu>
2019-05-16 17:07:03 -0700

    Updated sample level, included UI Scene

---
+++ Assets/MyScripts/HealthDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public int startingHealth = 5;
+    public int currentHealth;
+    public Text healthText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    currentHealth = startingHealth;
+}
+
+    // Update is called once per frame
+    void Update()
+{
+    healthText.text = "POTIONS: " + currentHealth;
+
+    if (Input.GetMouseButtonDown(0))
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth--;
+        }
+
+        else {
+                currentHealth = 0;
+                }
+    }
+
+}
+}
