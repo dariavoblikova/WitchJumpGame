a21734cc96632f8a2ba683a90d1f949c9a223e87
Joe V <joev@uw.edu>
2019-05-21 13:38:18 -0700

    Restructured project and first sample level build

---
+++ Assets/Original/MyScripts/MyLoadUILevelLeftClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyLoadUILevelLeftClick : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Ui");
+    }
+
+}
